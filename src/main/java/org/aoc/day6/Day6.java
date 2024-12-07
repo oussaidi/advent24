422b5c53078fb3b42ad484a3ac6a03ccd72c01f2
oussaidi <o.saidi21@gmail.com>
2024-12-07 19:01:28 +0100

    advent: day 6 DONE

--- src/main/java/org/aoc/day6/Day6.java
+++ src/main/java/org/aoc/day6/Day6.java
@@ -39,5 +39,50 @@ public class Day6 {
         throw new RuntimeException("Guard not found");
     }
 
+    public int countLoopInducingObstaclePositions(List<String> strings) {
+        int count = 0;
+        for (int i = 0; i < strings.size(); i++) {
+            List<String> stringsCopy = new ArrayList<>(strings);
+            for (int j = 0; j < strings.get(i).length(); j++) {
+                if(strings.get(i).charAt(j) != '#' && strings.get(i).charAt(j) != GUARD.charAt(0)) {
+                    stringsCopy.set(i, strings.get(i).substring(0, j) + '#' + strings.get(i).substring(j+1));
+                    if(matrixHasALoop(stringsCopy)) {
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
 
+
+    private boolean matrixHasALoop(List<String> strings) {
+        Set<Position> visitedCells = new HashSet<>();
+        int loopDetector = 0;
+        Direction currentDirection = Direction.UP;
+        Position currentPosition = findGuardInitialPosition(new ArrayList<>(strings));
+        visitedCells.add(currentPosition);
+
+        while (true) {
+            Position nextPosition = currentPosition.move(currentDirection);
+            if(nextPosition.x() < 0 || nextPosition.x() >= strings.size()
+                    || nextPosition.y() < 0 || nextPosition.y() >= strings.getFirst().length()) {
+                return false;
+            }
+            if (isObstacle(strings, nextPosition)) {
+                currentDirection = currentDirection.turnRight();
+            } else {
+                currentPosition = nextPosition;
+                if(visitedCells.contains(currentPosition)) {
+                    loopDetector++;
+                    if(loopDetector == visitedCells.size()) {
+                        return true;
+                    }
+                }else {
+                    loopDetector = 0;
+                }
+                visitedCells.add(currentPosition);
+            }
+        }
+    }
 }
--- src/test/java/org/aoc/Day6Test.java
+++ src/test/java/org/aoc/Day6Test.java
@@ -11,4 +11,10 @@ public class Day6Test {
         Day6 day6 = new Day6();
         System.out.println(day6.computeTotalVisitedCells(readFile("src/test/resources/day6.txt")));
     }
+
+    @Test
+    void count_loop_inducing_obstacle_positions() {
+        Day6 day6 = new Day6();
+        System.out.println(day6.countLoopInducingObstaclePositions(readFile("src/test/resources/day6.txt")));
+    }
 }
